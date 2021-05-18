73e7f2720b4645a200146bd1c7cd791a43bca9af
vlondonoo <vlondonoo@gmail.com>
2021-05-17 23:46:41 -0500

    feat: save images

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,55 +1,78 @@
 const { TodoModel } = require('../db/sequelize');
 const todo = require('../models/todo');
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
 
-exports.list = async function (req, res){
+exports.list = async function (req, res) {
+
   const todos = await TodoModel.findAll({
     order: [
       ['message', 'ASC'], // Sorts by COLUMN_NAME_EXAMPLE in ascending order
-],
-  }) ;
-  res.render("todo/list", {todos: todos});
-  
+    ],
+
+  });
+  let rsp = todos.filter(todo => todo.state === 'OPEN')
+  res.render("todo/list", { todos: todos });
 }
 
 exports.add = (req, res) => {
   // Validate request
-  if (!req.body.message) {
-    res.status(400).send({
-      message: "Content can not be empty!"
-    });
-    return;
-  }
   id = Math.floor((Math.random() * 100) + 1);
   state = "OPEN";
-  // Create a task
-  const todo = {
-    id,
-    message: req.body.message,
-    state
-  };
+  let newpath = '';
 
-  // Save task in the database
-  TodoModel.create(todo)
-    .then(data => {
-      console.log('dataaaa',data)
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Task."
+  // Read file imcomming 
+
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, function (err, fields, files) {
+
+    // Validar q venga un message
+    if (fields.message == '') {
+      res.status(400).send({
+        message: "Content can not be empty!"
       });
-    });    
-   res.redirect("/todos");
+      return;
+    } else {
+      if (files.filetoupload.name != '') {
+        console.log(`Files ${JSON.stringify(files)}\nfields ${JSON.stringify(fields)}\nError${err}`);
+        const oldpath = files.filetoupload.path;
+        const dirPath = path.join(__dirname.replace('controllers', ''), '/public/img/');
+        newpath = dirPath + files.filetoupload.name;
+        fs.rename(oldpath, newpath, function (err) {
+          if (err) throw err;
+        });
+      }
+      // Create a task
+      const todo = {
+        id,
+        message: fields.message,
+        state
+      };
+
+      // Save task in the database
+      TodoModel.create(todo)
+        .then(data => {
+          res.send(data);
+        })
+        .catch(err => {
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while creating the Task."
+          });
+        });
+      res.redirect("/todos");
+    }
+  });
 };
 
 
-exports.findOne = (req, res) => {
+exports.findOne = async (req, res) => {
   const id = req.params.id;
-
-  TodoModel.findByPk(id)
+  await TodoModel.findByPk(id)
     .then(data => {
-      res.send(data);
+      res.send(data.dataValues)
     })
     .catch(err => {
       res.status(500).send({
@@ -60,7 +83,6 @@ exports.findOne = (req, res) => {
 
 exports.delete = (req, res) => {
   const id = req.params.id;
-
   TodoModel.destroy({
     where: { id: id }
   })
@@ -80,27 +102,21 @@ exports.delete = (req, res) => {
         message: "Could not delete Task with id=" + id
       });
     });
-   res.redirect("/todos");
+  res.redirect("/todos");
 };
 
-exports.update= (req, res) => {
+exports.update = (req, res) => {
   const id = req.params.id;
-  
-  state = "OPEN";
-  
+  const state = req.params.state;
   const todo = {
-    id,
-    message: req.body.message,
     state
   };
-  TodoModel.update(req.body, {
+  TodoModel.update(todo, {
     where: { id: id }
   })
     .then(num => {
       if (num == 1) {
-        res.send({
-          message: "Task was updated successfully."
-        });
+        res.redirect("/todos");
       } else {
         res.send({
           message: `Cannot update Task with id=${id}. Maybe Task was not found or req.body is empty!`
@@ -109,7 +125,7 @@ exports.update= (req, res) => {
     })
     .catch(err => {
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
+        message: "Error updating Task with id=" + id
       });
     });
 };
@@ -117,25 +133,5 @@ exports.update= (req, res) => {
 
 
 
-exports.markCompleted = function(req, res, next) {
-  if (!req.body.completed) return next(new Error('Param is missing.'));
-  var completed = req.body.completed === 'true';
-  req.db.tasks.updateById(req.task._id, {$set: {completeTime: completed ? new Date() : null, completed: completed}}, function(error, count) {
-    if (error) return next(error);
-    if (count !==1) return next(new Error('Something went wrong.'));
-    console.info('Marked task %s with id=%s completed.', req.task.name, req.task._id);
-    res.redirect('/todos');
-  })
-};
-
-/*exports.completed = function(req, res, next) {
-  req.db.tasks.find({completed: true}).toArray(function(error, tasks) {
-    res.render('tasks_completed', {
-      title: 'Completed',
-      tasks: tasks || []
-    });
-  });
-};
 
 
-};*/
--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const homeController = require("./controllers/homeController");
 const todoController = require("./controllers/todoController");
 
 const app = express();
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /* Routes */
 app.get('/', homeController.home);
@@ -18,9 +18,10 @@ app.get('/about', homeController.about);
 app.get('/todos', todoController.list);
 app.post('/todos', todoController.add);
 
-//app.post('/todos/:task_id', todoController.findOne);
+app.get('/todos/findone/:id', todoController.findOne);
 app.post('/delete/todo/:id', todoController.delete);
-/*app.get('/todos/completed', todoController.completed);*/
+app.post('/todo/update/:id/:state', todoController.update);
+
 
 
 app.use(express.static("public"));
