a2581de1b8ffed9a28ce0fcb22fad36cb53948c4
Daniel Correa <yo@danielgara.com>
2021-05-24 13:19:30 -0500

    imp: new version with proper code

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,149 +1,61 @@
 const { TodoModel } = require('../db/sequelize');
-const todo = require('../models/todo');
-const formidable = require('formidable');
 const fs = require('fs');
 const path = require('path');
 
 exports.list = async function (req, res) {
-
-  const todos = await TodoModel.findAll({
-    order: [
-      ['message', 'ASC'],
-    ],
-  });
+  const todos = await TodoModel.findAll();
   res.render("todo/list", { todos: todos });
 }
 
-exports.add = (req, res) => {
-  // Validate request
-  id = Math.floor((Math.random() * 100) + 1);
-  state = "OPEN";
-  let newpath = '';
-  let image = ''
+exports.listOrdered = async function (req, res) {
+  const todos = await TodoModel.findAll({ order: [['message', 'ASC'],],});
+  res.render("todo/list", { todos: todos });
+}
 
-  // Read file imcomming 
-  const form = new formidable.IncomingForm();
-
-  form.parse(req, function (err, fields, files) {
-    if (fields.message == '') {
-      res.status(400).send({
-        message: "Content can not be empty!"
-      });
-      return;
-    } else {
-      if (files.filetoupload.name != '') {
-        console.log(`Files ${JSON.stringify(files)}\nfields ${JSON.stringify(fields)}\nError${err}`);
-        const oldpath = files.filetoupload.path;
-        const dirPath = path.join(__dirname.replace('controllers', ''), '/public/img/');
-        newpath = dirPath + files.filetoupload.name;
-        fs.rename(oldpath, newpath, function (err) {
-          if (err) throw err;
-        });
-        image = files.filetoupload.name
-      }
-      // Create a task
-
-      const todo = {
-        id,
-        message: fields.message,
-        state,
-        image
-      };
-
-      // Save task in the database
-      TodoModel.create(todo)
-        .then(data => {
-          res.send(data);
-        })
-        .catch(err => {
-          res.status(500).send({
-            message:
-              err.message || "Some error occurred while creating the Task."
-          });
-        });
-      res.redirect("/todos");
-    }
+exports.add = async (req, res) => {
+  const newTodo = await TodoModel.create({ 
+    message: req.body.message,
+    state: "OPEN",
+    image: "",
   });
+
+  const fileToUpload = req.files[0];
+
+  if (fileToUpload) {
+    const oldpath = fileToUpload.path;
+    const dirPath = path.join(__dirname.replace('controllers', ''), '/public/uploads/');
+    newpath = dirPath + newTodo.id + ".jpg";
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) throw err;
+    });
+    newTodo.image = newTodo.id + ".jpg";
+    await newTodo.save({ fields: ['image'] });
+  }
+
+  return res.redirect("/todos");
 };
 
-
-exports.findOne = async (req, res) => {
+exports.delete = async (req, res) => {
   const id = req.params.id;
-  await TodoModel.findByPk(id)
-    .then(data => {
-      res.send(data.dataValues)
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving Task with id=" + id
-      });
-    });
-};
+  await TodoModel.destroy({where:{ id: id}});
+  const imagePath = path.join(__dirname.replace('controllers', ''), '/public/uploads/');
+  newpath = imagePath + id + ".jpg";
+  try {
+    fs.unlinkSync(newpath) //file removed
+  } catch (err) {
+    //nothing
+  }
 
-exports.delete = (req, res) => {
-  const id = req.params.id;
-  const image = req.params.image;
-
-  TodoModel.destroy({
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        if (image != '') {
-
-          const imagePath = path.join(__dirname.replace('controllers', ''), '/public/img/');
-          newpath = imagePath + image;
-          try {
-            fs.unlinkSync(newpath)
-            //file removed
-          } catch (err) {
-            console.error(err)
-          }
-        }
-        res.send({
-          message: "Task was deleted successfully!"
-        });
-      } else {
-        res.send({
-          message: `Cannot delete Task with id=${id}. Maybe Task was not found!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Could not delete Task with id=" + id
-      });
-    });
   res.redirect("/todos");
 };
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   const id = req.params.id;
   const state = req.params.state;
-  const todo = {
+  const updatedTodo = {
     state
   };
-  TodoModel.update(todo, {
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.redirect("/todos");
-      } else {
-        res.send({
-          message: `Cannot update Task with id=${id}. Maybe Task was not found or req.body is empty!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating Task with id=" + id
-      });
-    });
+
+  await TodoModel.update(updatedTodo, {where:{id: id}});
+  res.redirect("/todos");
 };
-
-
-
-
-
-
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const multer = require('multer');
 
 /* include controllers */
 const homeController = require("./controllers/homeController");
@@ -9,22 +10,18 @@ const todoController = require("./controllers/todoController");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer({dest:'/public/uploads/'}).any());
 
 /* Routes */
 app.get('/', homeController.home);
 app.get('/about', homeController.about);
 app.get('/todos', todoController.list);
+app.get('/todosOrdered', todoController.listOrdered);
 app.post('/todos', todoController.add);
-
-app.get('/todos/findone/:id', todoController.findOne);
-app.post('/delete/todo/:id/:image', todoController.delete);
-app.post('/todo/update/:id/:state', todoController.update);
-
-
+app.get('/todos/delete/:id', todoController.delete);
+app.get('/todos/update/:id/:state', todoController.update);
 
 app.use(express.static("public"));
 app.listen(3000);
-
-
